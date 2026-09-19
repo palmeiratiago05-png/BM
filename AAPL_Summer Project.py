394488829bc47b549970a43a86f7f9d7a8172410
palmeiratiago05-png <palmeiratiago05@gmail.com>
2026-09-19 19:36:02 +0100

    Refactor to use yfinance for stock data import
    Refactor code to import Apple and NVIDIA stock data using yfinance. Added functions to import and clean data, and save the cleaned data to an Excel file.

--- AAPL_Summer Project.py
+++ AAPL_Summer Project.py
@@ -1,14 +1,27 @@
 import pandas as pd
-import numpy as np
+import yfinance as yf
 
-apple_stock = pd.read_csv("Apple_stock_history.csv")
-appl = apple_stock
-appl = appl["Open"].agg(["mean","sum","max","min"])
+def import_data():
+    data_import = yf.download(
+        ["AAPL","NVDA"],
+        period="6d",
+        interval= "15m",
+        group_by="ticker"
+    )
+    data_import.index = data_import.index.tz_localize(None)   # agora afeta o df que realmente vais gravar
 
-### Full display
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-print(appl)
+    return data_import
+
+def clean_data(import_data):
+    cleaner = import_data.dropna()
+    return cleaner
+
+
+## Conectar os dados 
+dados_brutos = import_data()
+dados_limpos = clean_data(dados_brutos)
+print(dados_limpos)
+dados_limpos.to_excel(r"C:\Users\__\Downloads\190September.xlsx")
 
 ### Data set for apple.csv https://www.kaggle.com/datasets/kalilurrahman/apple-stock-data-live-and-latest-from-ipo-date?select=Apple_stock_history.csv
 ### Or this site for apple https://www.kaggle.com/datasets/kalilurrahman/apple-stock-data-live-and-latest-from-ipo-date?resource=download
